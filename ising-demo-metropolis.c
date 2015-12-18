a9fecbec900be97bf8ae95b6eeb8ad4f8758d47f
MTariq <mubassartariq@gmail.com>
2015-12-18 14:55:42 -0500

    edited gitignore and indent pro

--- ising-demo-metropolis.c
+++ ising-demo-metropolis.c
@@ -11,23 +11,24 @@ int main (int argc, char **argv)
 {
     struct ising model;
 
-    double t = 2.;                   // temperature (kT)
-    double h = 0.;                   // magnetic field
-    double j = 1.;                   // ferromagnetic coupling
-    double tmin = 2., tmax = 3.;//, t;
-    int lx = 32;
+    double t;                   // temperature (kT)
+    double h = 0.;              // magnetic field
+    double j = 1.;              // ferromagnetic coupling
+    double tmin = 3., tmax = 5.;
+    int lx = 64;
     int ly = lx;
 
-    int mcsteps = 1024 * 128;        // 2^{17}
+    int mcsteps = 1024 * 128;   // 2^{17}
     int nt = 20;
-    double dt = (tmax - tmin)/(nt - 1);
+    double dt = (tmax - tmin) / (nt - 1);
+
     t = tmin;
 
-    if( ising_init (&model, lx, ly, j, h, t) != 0)
+    if (ising_init (&model, lx, ly, j, h, t) != 0)
     {
-        fprintf(stderr, "%s - cannot allocate spin array\n", argv[0]);
+        fprintf (stderr, "%s - cannot allocate spin array\n", argv[0]);
         argc = 1;
-        exit(1);
+        exit (1);
     }
 
     print_header (&model, stderr, tmin, tmax);
@@ -35,68 +36,70 @@ int main (int argc, char **argv)
     int therm_steps = mcsteps / 4;
     int report = therm_steps / 32;
 
-    for(int i = 0; i < nt; i++){
-    for (int s = 1; s <= therm_steps; s++)
+    for (int i = 0; i < nt; i++)
     {
-        (void) one_metropolis_step_per_spin (&model);
-        if (s % report == 0)
+        for (int s = 1; s <= therm_steps; s++)
         {
-            text_progress_bar (s, therm_steps, 32, "Thermalization", stderr);
+            (void) one_metropolis_step_per_spin (&model);
+            if (s % report == 0)
+            {
+                text_progress_bar (s, therm_steps, 32, "Thermalization",
+                    stderr);
+            }
         }
-    }
-    fprintf (stderr, " Done\n");
+        fprintf (stderr, " Done\n");
 
-    report = mcsteps / 32;
-    double mav = 0., m2av = 0., eav = 0., e2av = 0.;
-    double cv, xi;
+        report = mcsteps / 32;
+        double mav = 0., m2av = 0., eav = 0., e2av = 0.;
+        double cv, xi;
 
-    for (int s = 1; s <= mcsteps; s++)
-    {
-        (void) one_metropolis_step_per_spin (&model);
-        double m = magnetization_per_spin (&model);
-        double e = energy_per_spin (&model);
-
-        mav += m;
-        m2av += m * m;
-        eav += e;
-        e2av += e * e;
-
-        if (s % report == 0)
+        for (int s = 1; s <= mcsteps; s++)
         {
-            text_progress_bar (s, mcsteps, 32, "Production", stderr);
+            (void) one_metropolis_step_per_spin (&model);
+            double m = magnetization_per_spin (&model);
+            double e = energy_per_spin (&model);
+
+            mav += m;
+            m2av += m * m;
+            eav += e;
+            e2av += e * e;
+
+            if (s % report == 0)
+            {
+                text_progress_bar (s, mcsteps, 32, "Production", stderr);
+            }
+
         }
+        fprintf (stderr, " Done\n\n");
 
-    }
-    fprintf (stderr, " Done\n\n");
+        mav /= mcsteps;
+        eav /= mcsteps;
+        m2av /= mcsteps;
+        e2av /= mcsteps;
 
-    mav /= mcsteps;
-    eav /= mcsteps;
-    m2av /= mcsteps;
-    e2av /= mcsteps;
+        // specific heat dE/dt
+        cv = (e2av - eav * eav) / (t * t);
 
-    // specific heat dE/dt
-    cv = (e2av - eav*eav)/(t*t); 
+        // magnetic susceptibility
+        xi = (m2av - mav * mav) / t;
 
-    // magnetic susceptibility
-    xi = (m2av - mav*mav)/t;
-
-    printf ("%.3f   %.3f     % f  %f     % f  %f\n", t, h, mav, 
-        xi, eav, cv);
-    t += dt;
-    ising_reinit(&model, h, t);
+        printf ("%.3f   %.3f     % f  %f     % f  %f\n", t, h, mav, xi, eav,
+            cv);
+        t += dt;
+        ising_reinit (&model, h, t);
     }
 
-    ising_free (model); 
+    ising_free (model);
 
     return 0;
 }
 
-void print_header (struct ising *model, FILE *fp, double tmin, double tmax)
+void print_header (struct ising *model, FILE * fp, double tmin, double tmax)
 {
     fprintf (fp, "\n");
     fprintf (fp, " Two-dimensional Ising model - Metropolis simulation\n");
     fprintf (fp, " ---------------------------------------------------\n");
-    fprintf (fp, " Tmin/J = %.3f  Tmax/J = %.3f  H/J = %.3f  %dx%d\n\n", tmin/model->J, tmax/model->J,
-        model->H/model->J, model->Lx, model->Ly);
+    fprintf (fp, " Tmin/J = %.3f  Tmax/J = %.3f  H/J = %.3f  %dx%d\n\n",
+        tmin / model->J, tmax / model->J, model->H / model->J, model->Lx,
+        model->Ly);
 }
-
